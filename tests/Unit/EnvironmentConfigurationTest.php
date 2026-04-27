d772e87684c11b59156883214b93efb0f67f7094
weitingshih <weitingshih@softnext-inc.com>
2026-04-27 20:10:25 +0800

    chore: pin mise php to 8.5.4

--- tests/Unit/EnvironmentConfigurationTest.php
+++ tests/Unit/EnvironmentConfigurationTest.php
@@ -1,7 +1,7 @@
 <?php
 
 test('the project requires php 8.4 in composer', function () {
-    $composerPath = __DIR__ . '/../../composer.json';
+    $composerPath = __DIR__.'/../../composer.json';
 
     expect(file_exists($composerPath))->toBeTrue();
 
@@ -11,9 +11,9 @@ test('the project requires php 8.4 in composer', function () {
     expect($composer['require-dev']['laravel/boost'])->toBe('^2.4');
 });
 
-test('the project pins php 8.4 in mise', function () {
-    $misePath = __DIR__ . '/../../mise.toml';
+test('the project pins php 8.5.4 in mise', function () {
+    $misePath = __DIR__.'/../../mise.toml';
 
     expect(file_exists($misePath))->toBeTrue();
-    expect(file_get_contents($misePath))->toContain('php = "8.4"');
+    expect(file_get_contents($misePath))->toContain('php = "8.5.4"');
 });
